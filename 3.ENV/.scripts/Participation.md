707290f6538da1ff4ce3b070c30f4dc203886b0f
Brice Robert <300098957@collegeboreal.ca>
2021-11-29 16:17:00 -0500

    :tada:

---
+++ 3.ENV/.scripts/Participation.md
@@ -0,0 +1,23 @@
+# Participation au 29-11-2021 16:16
+
+| Table des matières            | Description                                             |
+|-------------------------------|---------------------------------------------------------|
+| :a: [Présence](#a-présence)   | L'étudiant.e a fait son travail    :heavy_check_mark:   |
+| :b: [Précision](#b-précision) | L'étudiant.e a réussi son travail  :tada:               |
+
+## Légende
+
+| Signe              | Signification                 |
+|--------------------|-------------------------------|
+| :heavy_check_mark: | Prêt à être corrigé           |
+| :x:                | Projet inexistant             |
+
+## :a: Présence
+
+|:hash:| Boréal :id:                | Interne            | :dvd: LVM |
+|------|----------------------------|--------------------|-----------|
+| 0 | 300111441 - <image src='https://avatars.githubusercontent.com/u/71027895?s=460&v=4' width=20 height=20></image> | :x: |
+| 1 | 300118524 - <image src='https://avatars.githubusercontent.com/u/73952068?s=460&v=4' width=20 height=20></image> | :x: |
+| 2 | 300117811 - <image src='https://avatars.githubusercontent.com/u/71027809?s=460&v=4' width=20 height=20></image> | :x: |
+| 3 | 300121460 - <image src='https://avatars.githubusercontent.com/u/71027883?s=460&v=4' width=20 height=20></image> | [:tada: 300121460](.300121460.rc) |
+| 4 | 300122014 - <image src='https://avatars.githubusercontent.com/u/71392439?s=460&v=4' width=20 height=20></image> | [:tada: 300122014](.300122014.rc) |
---
+++ 3.ENV/.scripts/participation.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# --------------------------------------
+#
+#
+#
+# --------------------------------------
+
+source ../.scripts/students.sh --source-only
+   
+echo "# Participation au `date +"%d-%m-%Y %H:%M"`"
+echo ""
+
+
+echo "| Table des matières            | Description                                             |"
+echo "|-------------------------------|---------------------------------------------------------|"
+echo "| :a: [Présence](#a-présence)   | L'étudiant.e a fait son travail    :heavy_check_mark:   |"
+echo "| :b: [Précision](#b-précision) | L'étudiant.e a réussi son travail  :tada:               |"
+
+echo ""
+echo "## Légende"
+echo ""
+
+echo "| Signe              | Signification                 |"
+echo "|--------------------|-------------------------------|"
+echo "| :heavy_check_mark: | Prêt à être corrigé           |"
+echo "| :x:                | Projet inexistant             |"
+
+
+echo ""
+echo "## :a: Présence"
+echo ""
+echo "|:hash:| Boréal :id:                | Interne            | :dvd: LVM |"
+echo "|------|----------------------------|--------------------|-----------|"
+
+i=0
+
+for id in "${ETUDIANTS[@]}"
+do
+   # --- config ---
+   ENV_FILE=.${id}.rc
+   # echo $ENV_FILE
+   OKI="| ${i} | ${id} - <image src='https://avatars.githubusercontent.com/u/${AVATARS[$i]}?s=460&v=4' width=20 height=20></image> | [:tada: ${id}](${ENV_FILE}) | "
+   KO="| ${i} | ${id} - <image src='https://avatars.githubusercontent.com/u/${AVATARS[$i]}?s=460&v=4' width=20 height=20></image> | :x: |"
+   # --- FILE -------------
+   if  [ -f "$ENV_FILE" ]; then
+       echo ${OKI}
+   else
+       echo ${KO}
+   fi
+   let "i++"
+done
